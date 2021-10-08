369217db494f552286c099566a4c67532f52a8d3
喂鱼 <chenweiyu233@gmail.com>
2021-10-08 20:34:16 +0800

    作业五 光追去噪
     光线追踪去噪，数据实现单帧降噪，两帧间的投影，两帧间的累积。案例：box与pink-room

--- homework5/src/denoiser.cpp
+++ homework5/src/denoiser.cpp
@@ -1,7 +1,73 @@
-#include "denoiser.h"
+﻿#include "denoiser.h"
 
 Denoiser::Denoiser() : m_useTemportal(false) {}
 
+
+//联合双边滤波
+Buffer2D<Float3> Denoiser::Filter(const FrameInfo &frameInfo) {
+    int height = frameInfo.m_beauty.m_height;
+    int width = frameInfo.m_beauty.m_width;
+    Buffer2D<Float3> filteredImage = CreateBuffer2D<Float3>(width, height);
+    int kernelRadius = 16;
+
+//是OpenMP中的一个指令，表示接下来的for循环将被多线程执行，另外每次循环之间不能有关系 collapse(2) 两重循环
+#pragma omp parallel for collapse(2)
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            // TODO: Joint bilateral filter
+
+            int x_start = std::max(0, x - kernelRadius);
+            int x_end = std::min(width - 1, x + kernelRadius);
+            int y_start = std::max(0, y - kernelRadius);
+            int y_end = std::min(height - 1, y + kernelRadius);
+            Float3 resColor;
+            auto weight_sum = .0f;
+            auto &center_postion = frameInfo.m_position(x, y);
+            auto &center_normal = frameInfo.m_normal(x, y);
+            auto &center_color = frameInfo.m_beauty(x, y);
+            for (int m = x_start; m <= x_end; m++) {
+                for (int n = y_start; n <= y_end; n++) {
+                    if (n == x && y == m) {
+                        weight_sum += 1.0f;
+                        resColor += center_color;
+                        continue;
+                    }
+
+                    auto &curr_postion = frameInfo.m_position(m, n);
+                    auto &curr_normal = frameInfo.m_normal(m, n);
+                    auto &curr_color = frameInfo.m_beauty(m, n);
+
+                    float plane_dis = .0f;
+                    auto position_dis =
+                        SqrDistance(center_postion, curr_postion) / (2.0f * m_sigmaCoord);
+                    auto color_dis =
+                        SqrDistance(center_color, curr_color) / (2.0f * m_sigmaColor);
+                    auto normal_dis = SafeAcos(Dot(center_normal, curr_normal)) /
+                                      (2.0f * m_sigmaNormal);
+                    if (position_dis != 0.f) {
+                        plane_dis =
+                            Dot(center_normal, Normalize(curr_postion - center_postion));
+                    }
+                    plane_dis = plane_dis * plane_dis / (2.0f * m_sigmaPlane);
+                    float weight =
+                        std::exp(-(plane_dis + position_dis + color_dis + normal_dis));
+                    weight_sum += weight;
+                    resColor += curr_color * weight;
+                }
+            }
+
+            // filteredImage(x, y) = frameInfo.m_beauty(x, y);
+            filteredImage(x, y) = resColor / weight_sum;
+        }
+    }
+    return filteredImage;
+}
+
+
+
+// &frameInfo 当前帧的信息 当前帧每个像素在上一帧的对应点，并将上一帧的结果投影到当前帧
+//这帧的world用逆矩阵回到模型空间再乘上上一帧的模型变换到上一帧的世界空间 再投影到屏幕上 
+//公式 Screeni?1 = Pi?1Vi?1Mi?1Mi?1Worldi
 void Denoiser::Reprojection(const FrameInfo &frameInfo) {
     int height = m_accColor.m_height;
     int width = m_accColor.m_width;
@@ -9,17 +75,41 @@ void Denoiser::Reprojection(const FrameInfo &frameInfo) {
         m_preFrameInfo.m_matrix[m_preFrameInfo.m_matrix.size() - 1];
     Matrix4x4 preWorldToCamera =
         m_preFrameInfo.m_matrix[m_preFrameInfo.m_matrix.size() - 2];
-#pragma omp parallel for
+#pragma omp parallel for collapse(2)
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
             // TODO: Reproject
             m_valid(x, y) = false;
             m_misc(x, y) = Float3(0.f);
+            int id = frameInfo.m_id(x,y);
+            if (id == -1) {
+                continue;
+            }
+            Matrix4x4 world_to_local = Inverse(frameInfo.m_matrix[id]);
+            const Matrix4x4 &pre_local_to_world = m_preFrameInfo.m_matrix[id];
+            auto postion = frameInfo.m_position(x,y);
+            auto screen_position = preWorldToScreen(pre_local_to_world(world_to_local(postion, Float3::EType::Point),
+                                   Float3::EType::Point),
+                Float3::EType::Point);
+            if (screen_position.x < 0 || screen_position.x>=width||
+                screen_position.y < 0 || screen_position.y >= height) {
+                continue;
+            } else {
+                int pre_x = screen_position.x;
+                int pre_y = screen_position.y;
+                int pre_id = m_preFrameInfo.m_id(pre_x,pre_y);
+                if (pre_id==id) {
+                    m_valid(x, y) = true;
+                    m_misc(x, y) = m_accColor(pre_x,pre_y);
+                }
+            }
         }
     }
     std::swap(m_misc, m_accColor);
 }
 
+
+//时间的积累  curFilteredColor降噪过的当前帧图像
 void Denoiser::TemporalAccumulation(const Buffer2D<Float3> &curFilteredColor) {
     int height = m_accColor.m_height;
     int width = m_accColor.m_width;
@@ -32,25 +122,13 @@ void Denoiser::TemporalAccumulation(const Buffer2D<Float3> &curFilteredColor) {
             // TODO: Exponential moving average
             float alpha = 1.0f;
             m_misc(x, y) = Lerp(color, curFilteredColor(x, y), alpha);
+
         }
     }
     std::swap(m_misc, m_accColor);
 }
 
-Buffer2D<Float3> Denoiser::Filter(const FrameInfo &frameInfo) {
-    int height = frameInfo.m_beauty.m_height;
-    int width = frameInfo.m_beauty.m_width;
-    Buffer2D<Float3> filteredImage = CreateBuffer2D<Float3>(width, height);
-    int kernelRadius = 16;
-#pragma omp parallel for
-    for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
-            // TODO: Joint bilateral filter
-            filteredImage(x, y) = frameInfo.m_beauty(x, y);
-        }
-    }
-    return filteredImage;
-}
+
 
 void Denoiser::Init(const FrameInfo &frameInfo, const Buffer2D<Float3> &filteredColor) {
     m_accColor.Copy(filteredColor);
@@ -60,7 +138,11 @@ void Denoiser::Init(const FrameInfo &frameInfo, const Buffer2D<Float3> &filtered
     m_valid = CreateBuffer2D<bool>(width, height);
 }
 
+
+
 void Denoiser::Maintain(const FrameInfo &frameInfo) { m_preFrameInfo = frameInfo; }
+
+
 
 Buffer2D<Float3> Denoiser::ProcessFrame(const FrameInfo &frameInfo) {
     // Filter current frame
@@ -82,3 +164,5 @@ Buffer2D<Float3> Denoiser::ProcessFrame(const FrameInfo &frameInfo) {
     }
     return m_accColor;
 }
+
+
--- homework5/src/denoiser.h
+++ homework5/src/denoiser.h
@@ -1,7 +1,8 @@
-#pragma once
+﻿#pragma once
 
 #define NOMINMAX
 #include <string>
+#include <algorithm>
 
 #include "filesystem/path.h"
 
@@ -10,14 +11,16 @@
 
 struct FrameInfo {
   public:
-    Buffer2D<Float3> m_beauty;
-    Buffer2D<float> m_depth;
-    Buffer2D<Float3> m_normal;
-    Buffer2D<Float3> m_position;
-    Buffer2D<float> m_id;
-    std::vector<Matrix4x4> m_matrix;
+    Buffer2D<Float3> m_beauty;  //代表渲染结果
+    Buffer2D<float> m_depth;    //代表每个像素的深度值  
+    Buffer2D<Float3> m_normal;  //代表每个像素的法线向量 (模长为 1)
+    Buffer2D<Float3> m_position;    //代表每个像素在世界坐标系中的位置
+    Buffer2D<float> m_id;           //代表每个像素对应的物体标号，对于没有物体的部分 (背景) 其标号为 - 1
+    std::vector<Matrix4x4> m_matrix;        //
 };
 
+
+//维护住历史帧的信息
 class Denoiser {
   public:
     Denoiser();
@@ -25,6 +28,7 @@ class Denoiser {
     void Init(const FrameInfo &frameInfo, const Buffer2D<Float3> &filteredColor);
     void Maintain(const FrameInfo &frameInfo);
 
+    //当前帧的信息通过函数 Denoiser::ProcessFrame(FrameInfo) 传入，并得到降噪后的图像
     void Reprojection(const FrameInfo &frameInfo);
     void TemporalAccumulation(const Buffer2D<Float3> &curFilteredColor);
     Buffer2D<Float3> Filter(const FrameInfo &frameInfo);
@@ -40,7 +44,7 @@ class Denoiser {
 
     float m_alpha = 0.2f;
     float m_sigmaPlane = 0.1f;
-    float m_sigmaColor = 0.6f;
+    float m_sigmaColor = 0.6f;  //  pink-room 10.6 box 0.6
     float m_sigmaNormal = 0.1f;
     float m_sigmaCoord = 32.0f;
     float m_colorBoxK = 1.0f;
